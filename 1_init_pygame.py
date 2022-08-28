f852691670b746b9aa1e4cbfee1d599e0420c660
Juan Fuenzalida <juan.fuenza97@icloud.com>
2022-08-28 19:53:51 -0300

    fix: pygame.quit indentation

--- 1_init_pygame.py
+++ 1_init_pygame.py
@@ -19,5 +19,5 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    # End game
-    pygame.quit()
+# End game
+pygame.quit()
--- 2_drawing_on_display.py
+++ 2_drawing_on_display.py
@@ -1,1 +1,36 @@
 import pygame
+
+# Init pygame
+pygame.init()
+
+# Display surface
+WIN_WIDTH = 600
+WIN_HEIGHT = 600
+display_surface = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
+pygame.display.set_caption("Drawing Objets")
+
+# Define colors as rgb tuples
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+CYAN = (0, 255, 255)
+MAGENTA = (255, 0, 255)
+
+# BG 
+display_surface.fill(BLUE)
+
+
+# Main game loop
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    pygame.display.update()
+
+# Endgame
+pygame.quit()
