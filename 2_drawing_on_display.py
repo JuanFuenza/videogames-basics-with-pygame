6881a491396301bd1dd4c11d2fb6de806105d61d
Juan Fuenzalida <juan.fuenza97@icloud.com>
2022-08-28 20:07:24 -0300

    feat: draw simple geometric figures

--- 2_drawing_on_display.py
+++ 2_drawing_on_display.py
@@ -22,6 +22,19 @@ MAGENTA = (255, 0, 255)
 # BG 
 display_surface.fill(BLUE)
 
+# Draw various shapes on our display
+# Line(Surface, color, starting point, ending point, thickness)
+pygame.draw.line(display_surface, RED, (0, 0), (100, 100), 5)
+pygame.draw.line(display_surface, GREEN, (100, 100), (200, 300), 1)
+
+# Circle(surface, color, center, radius, thickness...0 for fill)
+pygame.draw.circle(display_surface, WHITE, (WIN_HEIGHT // 2, WIN_WIDTH // 2), 200, 6)
+pygame.draw.circle(display_surface, YELLOW, (WIN_HEIGHT // 2, WIN_WIDTH // 2), 195, 0)
+
+# Rectangle(surface, color, (top-left x, top-left-y, width, height))
+pygame.draw.rect(display_surface, CYAN, (500, 0, 100, 100))
+pygame.draw.rect(display_surface, MAGENTA, (500, 100, 50, 100))
+
 
 # Main game loop
 running = True
@@ -30,6 +43,7 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
+    # Update screen
     pygame.display.update()
 
 # Endgame
